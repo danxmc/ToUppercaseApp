f097fa3c326f4f613478a7ae8cb43bd72d7184dc
danx <danx_97@hotmail.com>
2019-05-27 19:22:59 -0500

    Add backend template files

---
+++ backend/routes/api/strings.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+// @route   POST api/strings
+// @desc    Convert the string to Uppercase
+// @access  Public
+router.post('/', (req, res) => {
+    
+});
+
+module.exports = router;
---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Bodyparser Middleware
+app.use(express.json());
+
+// Use Routes
+app.use('/api/string', require('./routes/api/strings'));
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('../frontend/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve('../frontend', 'build', 'index.html'));
+    });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
